55442297ba49ce1fcf11fee3d0885beafb02a70c
Peter McKinley <peter.michael.mckinley@gmail.com>
2017-06-06 12:00:56 -0400

    Removed isPrime from primes

--- problems/primes.js
+++ problems/primes.js
@@ -1,8 +1,5 @@
 // primes(num) returns an array of the first "num" primes.
-// You may wish to use an is_prime? helper method.
-function isPrime(num) {
-
-}
+// You may wish to use an is_prime helper method.
 
 function primes(num) {
 
