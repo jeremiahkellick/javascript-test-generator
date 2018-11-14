df91c511d01bc29530e1aafa8d642ef26d130b9d
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-11-14 15:37:09 -0800

    Improve baseConverter solution

--- solutions/base_converter_solution.js
+++ solutions/base_converter_solution.js
@@ -1,10 +1,5 @@
 function baseConverter(num, b) {
-  if (num === 0) return "";
-
-  const digits = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f'
-  ];
-
-  return baseConverter(Math.floor(num/b), b) + digits[num % b];
-};
+  if (num < 1) return '';
+  const digits = '0123456789abcdef';
+  return baseConverter(Math.floor(num / b), b) + digits[num % b];
+}
