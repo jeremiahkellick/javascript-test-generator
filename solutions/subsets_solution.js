1ad0af77c169aee8b10659994427772adf089863
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-11-14 13:27:11 -0800

    Improve subsets solution

--- solutions/subsets_solution.js
+++ solutions/subsets_solution.js
@@ -1,10 +1,6 @@
-function subsets(arr) {
-  if (!arr.length) return [[]];
-  const last = arr[arr.length - 1];
-  const subs = subsets(arr.slice(0, arr.length - 1));
-  return subs.concat(subs.map( (el) => {
-    let newArr = el.slice(0)
-    newArr.push(last);
-    return newArr;
-  }));
+function subsets(array) {
+  if (array.length === 0) return [[]];
+  const prevSubs = subsets(array.slice(0, array.length - 1));
+  const last = array[array.length - 1];
+  return prevSubs.concat(prevSubs.map(sub => sub.concat(last)));
 }
